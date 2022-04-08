87e89f0faef3bd19bee7f977d87527013b52cc96
Pasindu Danthanarayana <65776520+pasindu651@users.noreply.github.com>
2022-04-08 09:27:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ num1 =0;
 num2 =0;
 signChoice = "";
 
-/*Function to add numbers to input (screen)*/
+//Function to add numbers to input (screen)
 function addNum(num){
 document.getElementById("screen").value = document.getElementById("screen").value + num;
 }
 
-/*Function to set num1 and to assign chosen operator to variable*/
+//Function to set num1 and to assign chosen operator to variable
 function setSign(sign){
 signChoice = sign;
 num1 = document.getElementById("screen").value;
@@ -16,7 +16,7 @@ console.log(num1);
 document.getElementById("screen").value ="";
 }
 
-/*Function that when equal is clicked, evaluates based on chosen operator*/
+//Function that when equal is clicked, evaluates based on chosen operator
 function equalPressed() {
 num2= document.getElementById("screen").value;
 if (signChoice =="+") {
@@ -32,49 +32,49 @@ document.getElementById("screen").value = Number(num1)+ Number(num2);
 }
 }
 
-/*Function to switch number to negative*/
+//Function to switch number to negative
 function switchCase() {
   document.getElementById("screen").value
   = -Math.abs(document.getElementById("screen").value);
 }
 
-/*Function to convert number to decimal out of 100*/
+//Function to convert number to decimal out of 100
 function percent() {
   document.getElementById("screen").value
   = (document.getElementById("screen").value) /100;
 }
 
-/*Function to square number*/
+//Function to square number
 function square() {
   document.getElementById("screen").value
   = (document.getElementById("screen").value) **2;
 }
 
-/*Function to square root number*/
+//Function to square root number
 function sqrt() {
   document.getElementById("screen").value
   = Math.sqrt(document.getElementById("screen").value);
 }
 
-/*Function to cut number in half*/
+//Function to cut number in half
 function half() {
   document.getElementById("screen").value
   = (document.getElementById("screen").value) /2;
 }
 
-/*Function to add decimal to number*/
+//Function to add decimal to number
 function decimal() {
   document.getElementById("screen").value
   = ((Number(document.getElementById("screen").value)).toFixed(1)).slice(0,-1);
 }
 
-/*Function to determine natural logarithm of number*/
+//Function to determine natural logarithm of number
 function log() {
   document.getElementById("screen").value
   = Math.log(document.getElementById("screen").value);
 }
 
-/*Function to reset input (screen)*/
+//Function to reset input (screen)
 function reset() {
   document.getElementById("screen").value = "";
 }
